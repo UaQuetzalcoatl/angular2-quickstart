1c262d4171342dbb4398730a0961c03886af7894
Alex <ua.quetzalcoatl@gmail.com>
2016-09-30 18:06:02 +0300

    wip

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+import { routing } from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [AppComponent, HeroDetailComponent],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule, routing, HttpModule ],
+  declarations: [AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
+  bootstrap: [AppComponent],
+  providers: [HeroService]
 })
 export class AppModule { }
--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,8 +1,13 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
+
 import {Hero} from './Hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
+  styleUrls: ['app/hero-detail.component.css'],
   template: `<div *ngIf="hero">
     <h2>{{hero.name}} details!</h2>
     <div><label>id: </label>{{hero.id}}</div>
@@ -10,9 +15,27 @@ import {Hero} from './Hero';
       <label>name: </label>
       <input [(ngModel)]="hero.name" placeholder="name"/>
     </div>
+    <button (click)="goBack()">Back</button>
   </div>`
 })
-export class HeroDetailComponent {
-  @Input()
+export class HeroDetailComponent implements OnInit {
+
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params.forEach((params: Params) => {
+      let id = +params['id'];
+      this.heroService.getHero(id).then(hero => this.hero = hero);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
